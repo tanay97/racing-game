6a03f2ba4c9f43ac078328532f0fbcc2497f3122
tanay97 <tanaydesai1997@gmail.com>
2016-09-25 14:38:52 -0400

    Add files via upload

---
+++ Constants.py
@@ -0,0 +1,47 @@
+# Animations
+DEFAULT_ANIMATION_SPEED = .1
+# Background
+BACKGROUND_TEXTURE = "data/background4.png"
+# Explosion
+EXPLOSION_IMAGE       ="data/explode-alpha/explode-alpha.png"
+EXPLOSION_SOUND       = "data/explode.wav"
+EXPLOSION_NUMFRAMES   = [30]
+EXPLOSION_SIZE        = [(100, 100)]
+
+# Difficulty
+
+DIFFICULTY = 4.0 # 1 being most difficult and 5 is least
+
+# Cars
+CAR_SIZE = (50, 100)
+TIRE_SKID = "data/tireskid.ogg"
+ENGINE_SOUND = "data/engine.ogg"
+
+PLAYER_CAR_IMAGE = "data/player-car.png"
+PLAYER_CAR_SPEED = 15
+PLAYER_CAR_TURNSPEED = 5
+PLAYER_CAR_ACCELERATION = -.1
+
+
+NORMAL_CAR_IMAGE = "data/car.png"
+NUM_NORMAL_CARS = 1
+NORMAL_CAR_SPEED = 10
+NORMAL_CAR_TURNSPEED = 8
+NORMAL_CAR_ACCELERATION = -10
+
+AI_CAR_IMAGE = "data/ai.png"
+NUM_AI_CARS = 1
+AI_CAR_SPEED = 25
+AI_CAR_TURNSPEED = 10
+AI_CAR_ACCELERATION = -.3
+
+
+
+# GUI
+WINDOW_TITLE          = "Racing Game"
+WINDOW_ICON           = "data/racing-game-icon.png"
+WINDOW_SIZE           = (600, 700)
+
+START = "data/start.png"
+FINISH = "data/finish.png"
+FRICTION              = 0.8
